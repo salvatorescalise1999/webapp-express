725654b089dd412b29abfb110e3b2f4cb45f6b8b
Salvatore Scalise <salvatorescalise1999@gmail.com>
2026-02-20 17:41:53 +0100

    feat: creato controller movie base

---
+++ controllers/movieController.js
@@ -0,0 +1,15 @@
+//importimao la connessione del DB
+const connection = require('../data/db')
+
+
+//funzione di index
+function index (req, res){
+
+}
+
+//funzione di show
+function show (req, res){
+
+}
+
+module.exports = {index, show}
