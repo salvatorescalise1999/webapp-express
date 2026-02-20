00e5a32079ab1ebcd368065b2c464193baf3bc42
Salvatore Scalise <salvatorescalise1999@gmail.com>
2026-02-20 19:25:43 +0100

    feat: completata rotta show movie con le reviews

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -23,20 +23,35 @@ function show(req, res) {
     // Prendo l'id dai parametri della richiesta e lo converto in numero
     const id = parseInt(req.params.id);
 
-    // prepariamo la query per la richista
-    const movieSql = 'SELECT * FROM movies WHERE id = ?'; 
+    // prepariamo la query per la richista del film
+    const movieSql = 'SELECT * FROM movies WHERE id = ?';
 
-    connection.query(movieSql, [id], (err, results) => {
+    // prepariamo la query per la richista della reviews
+    const reviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?';
+
+    // chiamamta a id principale per recuperare il libro
+    connection.query(movieSql, [id], (err, movieResults) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
-        if (results.length === 0) return res.status(404).json({ error: 'Movie not found' });
+        if (movieResults.length === 0) return res.status(404).json({ error: 'Movie not found' });
 
         // Salviamo il risultato in una costante
-        const movie = results[0];
+        const movie = movieResults[0];
 
-        // ritorniamo il JSON del film
-        res.json(movie);
+        // chiammata  id secondaria per recupero reviews del film
+        connection.query(reviewsSql, [id], (err, reviewsResults) => {
+            if (err) return res.status(500).json({ error: 'Database query failed' });
+            
+            // salvaiamo le reviews in una costante
+            const reviewsArray = reviewsResults;
+
+            // Aggiungiamo all'oggetto film la proprietà per le reviews
+            movie.reviews = reviewsArray;
+
+            // ritorniamo il JSON del film
+            res.json(movie);
+        });
+
     });
-
 }
 
 module.exports = { index, show }
